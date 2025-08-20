72871985c190f93f967b2647acdfce0eae3f7f8f
Tomasz Ayobahan <tomasz.ayobahan@gmail.com>
2025-08-20 17:22:47 -0600

    header and image resources

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/products';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  // protected readonly title = signal('client');
+export class AppComponent implements OnInit{
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'Skinet';
-}
+  products: Product[] = [];
+
+  ngOnInit(): void {
+      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+        next: response => this.products = response.data,
+        error: error => console.log(error),
+        complete: () => console.log('complete')
+      })
+    }
+  }
