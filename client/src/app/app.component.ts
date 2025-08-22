d89caabc324a78358aa759c560f1b1f4654b7b97
Tomasz Ayobahan <tomasz.ayobahan@gmail.com>
2025-08-22 15:07:03 -0600

    routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
