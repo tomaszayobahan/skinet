1ba06f73401923d558178bd9b0e8b53a0ff08d8d
Tomasz Ayobahan <tomasz.ayobahan@gmail.com>
2025-08-23 16:46:33 -0600

    get rid if unused imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,11 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/products';
-import { Pagination } from './shared/models/pagination';
-import { ShopService } from './core/services/shop.service';
-import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
